71f3a65c8277028c6ca0d93a2415db135de67804
AxelWhite87 <78832548+AxelWhite87@users.noreply.github.com>
2022-04-13 20:40:48 -0400

    Comitting changes

---
+++ src/app/controllers/api/stories.controller.ts
@@ -0,0 +1,26 @@
+import { Context, Delete, Get, HttpResponseCreated, HttpResponseNoContent, HttpResponseNotFound, HttpResponseOK, Post, UserRequired, ValidateBody, ValidatePathParam, ValidateQueryParam } from '@foal/core';
+import { Story, User } from '../../entities';
+
+export class StoriesController {
+  @Get()
+  @ValidateQueryParam('authorId', { type: 'number' }, { required: false })
+  async readStories(ctx: Context){
+    const authorId = ctx.request.query.authorId as number|undefined;
+    let queryBuilder = Story
+        .createQueryBuilder('story')
+        .leftJoinAndSelect('story.author', 'author')
+        .select([
+            'story.id',
+            'story.title',
+            'story.link',
+            'author.id',
+            'author.name'
+        ]);
+
+    if (authorId !== undefined){
+      queryBuilder = queryBuilder.where('author.id = :authorId', {authorId})
+    }
+    const stories = await queryBuilder.getMany();
+    return new HttpResponseOK(stories);
+  }
+}
--- src/app/entities/user.entity.ts
+++ src/app/entities/user.entity.ts
@@ -1,5 +1,5 @@
 // import { hashPassword } from '@foal/core';
-import { BaseEntity, /*Column, */Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User extends BaseEntity {
@@ -7,14 +7,22 @@ export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  // @Column({ unique: true })
-  // email: string;
+  @Column({ unique: true })
+  email: string;
 
-  // @Column()
-  // password: string;
+  @Column()
+  password: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  avatar: string;
 
   // async setPassword(password: string) {
   //   this.password = await hashPassword(password);
   // }
 
 }
+
+export { DatabaseSession } from '@foal/typeorm';
