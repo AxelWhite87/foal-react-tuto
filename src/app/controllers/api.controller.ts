7c233a9feeac5f69d9079617e11096a5880c0aff
AxelWhite87 <78832548+AxelWhite87@users.noreply.github.com>
2022-04-13 20:44:42 -0400

    API server setup

--- src/app/controllers/api.controller.ts
+++ src/app/controllers/api.controller.ts
@@ -1,5 +1,14 @@
-import { Context, controller, Get, HttpResponseOK } from '@foal/core';
+import { ApiInfo, ApiServer,
+  Context, controller, Get, HttpResponseOK } from '@foal/core';
 import { StoriesController } from './api';
+
+@ApiInfo({
+  title: 'Application API',
+  version: '1.0.0'
+})
+@ApiServer({
+  url: '/api'
+})
 
 export class ApiController {
   subControllers = [
